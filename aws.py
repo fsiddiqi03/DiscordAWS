8c23d3a95d1ab3504a1546fd4f91ca607ec0aada
Faris Siddiqi <farisiddiqi@gmail.com>
2023-10-23 15:02:40 -0500

    added test function for SSM connection

--- aws.py
+++ aws.py
@@ -7,6 +7,7 @@ class EC2Manager:
     def __init__(self, instance_id="i-0c1ded99c0ff7c3b8", region="us-east-2"):
         self.instance_id = instance_id
         self.ec2 = boto3.client("ec2", region_name=region)
+        self.ssm = boto3.client("ssm", region_name=region)
 
     def check_ec2_status(self):
         response = self.ec2.describe_instances(InstanceIds=[self.instance_id])
@@ -51,10 +52,50 @@ class EC2Manager:
 
     #def start_server(self):
         # script to start the minecraft server 
-        # should call the start ec2 method
+        # should check if the ec2 instance is on, if not should call the start ec2 method
         # when the start_ec2 instance returns running, run the script command to launch the server. 
     
+    
+    def test_ssm_connection(self):
+        try:
+            response = self.ssm.send_command(
+                InstanceIds=[self.instance_id],
+                DocumentName="AWS-RunShellScript",
+                Parameters={
+                    'commands': ['echo "Connection Test Successful!"']
+                }
+            )
 
+            command_id = response['Command']['CommandId']
+            time.sleep(5)  # Sleep for 5 seconds
+
+            result = self.ssm.list_command_invocations(
+                CommandId=command_id,
+                InstanceId=self.instance_id,
+                Details=True
+            )
+
+            # Check if the command succeeded
+            if result['CommandInvocations'][0]['Status'] == 'Success':
+                output = result['CommandInvocations'][0]['CommandPlugins'][0]['Output']
+                print(output)
+                return True
+            else:
+                print("Failed to connect via SSM.")
+                return False
+        except Exception as e:
+            print(f"Error during SSM connection test: {e}")
+            return False
+
+    # Your other methods for starting the server and automatic turn-off...
+
+ec2_manager = EC2Manager()
+
+if ec2_manager.test_ssm_connection():
+     print("SSM connection successful!")
+     # Here you can call your method to start the Minecraft server
+else:
+    print("Failed to connect via SSM.")
  
     
 
@@ -62,11 +103,6 @@ class EC2Manager:
         # check the minecraft server to see if any players are online every 30 mins, if there is no one turn off the ec2 instance. 
 
 
-
-ec2_manager = EC2Manager()
-
-ip = ec2_manager.get_ip()
-print(ip)
 
 
 
