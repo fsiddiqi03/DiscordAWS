cee74c1291262edb17fe61a0290832faee745eda
Faris Siddiqi <farisiddiqi@gmail.com>
2023-12-28 12:14:23 -0600

    added setup script for rasberrypi

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Update and upgrade the system
+sudo apt-get update
+sudo apt-get upgrade -y
+
+# Install Python3 and pip
+sudo apt-get install python3 python3-pip -y
+
+# Install discord.py library 
+pip3 install discord.py
+
+# Install additional Python libraries 
+pip3 install boto3
+pip3 install mcstatus
+pip3 install mcrcon
+pip3 install randfacts
+
+
+# alert user that setup is complete 
+echo "Setup complete. All necessary libraries installed."
